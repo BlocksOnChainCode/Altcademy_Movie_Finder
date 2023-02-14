6eed5688c86ac8aa7236c166ee6f6e986eaa4da5
Arno luyckx <A.luyckx@protonmail.com>
2023-02-14 14:41:44 +0100

    Fully functional, now on to styling

--- main.js
+++ main.js
@@ -14,12 +14,69 @@
  * 
  * 
  */
+
+const httpRequest = new XMLHttpRequest();
+
+httpRequest.onload = function() {
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      const response = JSON.parse(httpRequest.responseText);
+      console.log(response);
+
+      const resultsDiv = document.getElementById("results");
+        console.log(resultsDiv.children)
+      for (let i = 0; i < response.Search.length; i++) {
+        const movie = response.Search[i];
+          console.log(movie);
+        const movieDiv = document.createElement("div");
+        movieDiv.className = "movie-card";
+        movieDiv.innerHTML = `<img class="movie-poster" src="${movie.Poster}" alt="${movie.Title}">
+                              <h2>${movie.Title}</h2>
+                              <p>Year: ${movie.Year}</p>
+                              <p>Type: ${movie.Type}</p>
+                              <p>imdbID: ${movie.imdbID}</p>`;
+        resultsDiv.appendChild(movieDiv);
+      }
+    } else {
+      console.log(httpRequest.statusText);
+    }
+  }
+};
+
+httpRequest.onerror = function() {
+  console.log(httpRequest.statusText);
+};
+
+const searchMovie = function() {
+  const results = document.getElementById('results');
+  const input = document.querySelector("input").value;
+  console.log(`Triggered ${input}`);
+
+  results.innerHTML = "";
+
+  if (input) {
+    httpRequest.open(
+      "GET",
+      `https://www.omdbapi.com/?s=${input}&plot=short&apikey=a67ee14e`
+    );
+    httpRequest.send();
+  }
+};
+
+
+
+
+
+
+
+/*
 const httpRequest = new XMLHttpRequest();
 httpRequest.onload = function() {
   if (httpRequest.readyState === XMLHttpRequest.DONE) {
     if (httpRequest.status === 200) {
       console.log(httpRequest.responseText);
       var movie = JSON.parse(httpRequest.responseText);
+      console.log(movie);
       document.getElementById("image").setAttribute('src', movie.Poster);
       document.getElementById("title").innerHTML = movie.Title;
       document.getElementById("year").innerHTML = movie.Year;
@@ -39,7 +96,7 @@ var searchMovie = function () {
   var input = document.querySelector('input').value;
   console.log(`Triggered ${input}`)
   if (input) {
-    httpRequest.open('GET', 'https://www.omdbapi.com/?t=' + input + '&plot=short&apikey=a67ee14e');
+    httpRequest.open('GET', 'https://www.omdbapi.com/?s=' + input + '&plot=short&apikey=a67ee14e');
     httpRequest.send();
   }
-}
+}*/
